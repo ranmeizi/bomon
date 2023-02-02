f06a66fefe87afad122c1866e0a6053dd5fe7a80
boboan <360969885@qq.com>
2023-02-03 00:58:46 +0800

    components project

---
+++ packages/dev-publish/rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+
+const plugin = [
+  typescript(),
+  commonjs()
+];
+const config = [
+  {
+    input: "src/index.ts",
+    plugins: plugin,
+    bundleConfigAsCjs: true,
+    output: [
+      {
+        file: "dist/index.js",
+        format: 'cjs'
+      }
+    ]
+  }
+];
+
+export default config;
