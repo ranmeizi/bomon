1db95339a3c3f31837e27532f57fc7f7ce41e458
boboan <360969885@qq.com>
2023-02-19 01:02:39 +0800

    delete file

--- packages/expand-router/src/index.tsx
+++ packages/expand-router/src/index.tsx
@@ -1,14 +1,10 @@
-import TransitionOutlet from './components/TransitionOutlet'
 import KRoute from './components/KRoute'
 import EventLRU from './utils/LRU'
 import TransitionRoute from './components/Transition/Route'
 import TransitionProvider from './components/Transition/Provider'
-import SwitchOutlet from './components/SwitchOutlet'
 export { AliveScope } from 'react-activation'
 
 export {
-    TransitionOutlet,
-    SwitchOutlet,
     KRoute,
     TransitionRoute,
     TransitionProvider,
